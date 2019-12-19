6f7d566fa2b813a9c2f475be6679936c69b1841a
Ryan Dollart <info@keyworkz.nl>
2019-12-19 16:59:01 +0100

    using bootstrapzero templates and routing

--- app.js
+++ app.js
@@ -5,32 +5,29 @@ const morgan = require('morgan');
 const path = require('path');
 
 // Second calling the app function
+// making use of my env and port(s)
 const app = express();
+const port = process.env.PORT || 3000;
 
 // serve static files from our public or our node modules
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(
-  '/css',
-  express.static(path.join(__dirname, 'node-modules/bootstrap/dist/css')),
-);
-app.use(
-  '/js',
-  express.static(path.join(__dirname, 'node-modules/bootstrap/dist/js')),
-);
-app.use(
-  '/js',
-  express.static(path.join(__dirname, 'node-modules/jquery/dist')),
-);
+app.use('/css', express.static(path.join(__dirname, 'node-modules/bootstrap/dist/css')),);
+app.use('/js', express.static(path.join(__dirname, 'node-modules/bootstrap/dist/js')),);
+app.use('/js', express.static(path.join(__dirname, 'node-modules/jquery/dist')),);
+
+// templating using appset.
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 // using morgan
 app.use(morgan('tiny'));
 
 // Third making use of the get function
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index', { list: ['a', 'b'], title:'Mayan Power!!!'});
 });
 
 // Fourth using the listen function as a webserver using express
-app.listen(3000, () => {
-  console.log(`Listening on port ${chalk.bold.green('3000')}`);
+app.listen(port, () => {
+  console.log(`Listening at port ${chalk.bold.green(port)}`);
 });
